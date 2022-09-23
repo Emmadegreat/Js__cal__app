99b746cd21b1d373901d3e83460f7ad7f2b0cbb4
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2022-09-23 19:09:32 +0100

    added

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-let result = document.getElementById("inputext");
+let input = document.getElementById("inputext");
 
 //function to display the input on each keypad
 function Calculate(number) {
-    result.value = result.value+number;
+    input.value = input.value+number;
 }
 
 //function to give the result/output of the calculation by clicking the equal key
 function Result(){
     try {
-        result.value = eval(result.value);
+        input.value = eval(input.value);
     } catch (error) {
         document.getElementById("inputext").value = "math error!";
-    } 
+    }
 }
 
 //function to clear everything on the input bar
 function ClearAll() {
-    result.value = "";
+    input.value = "";
 }
 
 //function to delete inputs on the input bar from the last to the first
 function Delete() {
-    result.value = result.value.slice(0, -1);
+    input.value = input.value.slice(0, -1);
+}
+
+function Sqr() {
+    input.value = input.value+Math.sqrt(number);
 }
